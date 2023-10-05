91cee0b56b0c89a4227bdc02a1d4a68793ad36b9
HiwotBerhanu <88843300+HiwotBerhanu@users.noreply.github.com>
2023-10-05 01:41:02 -0700

    print popup

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,12 +5,14 @@ const {getOneAdmin}=require('../controllers/admin')
 const {createAdmin}=require('../controllers/admin')
 const {updateAdmin}=require('../controllers/admin')
 const {deleteAdmin}=require('../controllers/admin')
+const { generatePDF } = require("../controllers/pdfGenerator");
 
 router.get('/getAll', getAllAdmins);
 router.get('/getOne/:id',getOneAdmin)
 router.post('/create',createAdmin);
 router.put('/update/:id',updateAdmin)
 router.delete('/delete/:id',deleteAdmin)
+router.post("/generatePdf", generatePDF);
 
 
 module.exports=router;
--- frontend/src/Components/Course/ViewPopup.js
+++ frontend/src/Components/Course/ViewPopup.js
@@ -1,27 +1,56 @@
 // import './Popup.css';
-import {React} from 'react';
+import React, { useRef } from "react";
+import axios from 'axios';
 export const ViewPopup=(props)=> {
-
+   
+  const popupRef = useRef();
 
   const courseProp=props.courseProp
   const setTrigger=props.setTrigger
   // console.log(courseProp)
   // const type=data.course_type
 
+ const handlePrint = async () => {
+   try {
+     const element = popupRef.current;
+     const htmlContent = element.innerHTML;
+
+     const response = await axios.post(
+       "http://localhost:8081/course/generatePdf",
+       { htmlContent },
+       {
+         responseType: "blob",
+       }
+     );
+
+     const pdfUrl = window.URL.createObjectURL(new Blob([response.data]));
+     const link = document.createElement("a");
+     link.href = pdfUrl;
+     link.setAttribute("download", "course.pdf");
+     document.body.appendChild(link);
+     link.click();
+     link.remove();
+   } catch (error) {
+     console.error("Error generating PDF:", error);
+   }
+ };
+
   
 
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <h5>Course's Id:</h5>
-        <p> {courseProp.id_tag}</p>
-        <h5>Course's Name:</h5>
-        <p> {courseProp.course_name} </p>
-        <h5>Course's Tuition fee:</h5>
-        <p> {courseProp.fee} </p>
-        <h5>Course's Duration in days:</h5>
-        <p> {courseProp.course_duration} </p>
-
+        <div ref={popupRef}>
+          {" "}
+          <h5>Course's Id:</h5>
+          <p> {courseProp.id_tag}</p>
+          <h5>Course's Name:</h5>
+          <p> {courseProp.course_name} </p>
+          <h5>Course's Tuition fee:</h5>
+          <p> {courseProp.fee} </p>
+          <h5>Course's Duration in days:</h5>
+          <p> {courseProp.course_duration} </p>
+        </div>
         <button
           className="close-btn  btn btn-secondary"
           onClick={() => {
@@ -30,12 +59,7 @@ export const ViewPopup=(props)=> {
         >
           close
         </button>
-        <button
-          className="print-btn btn btn-warning"
-          // onClick={() => {
-          //   setTrigger(false);
-          // }}
-        >
+        <button className="print-btn btn btn-warning" onClick={handlePrint}>
           print
         </button>
         {props.children}
